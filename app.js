7ea589b900ff65994066151b808ff579b83f6b71
Andrew Li <andrewli@andrews-mbp.lan>
2019-05-28 23:57:43 -0700

    first working version

---
+++ app.js
@@ -0,0 +1,121 @@
+var cards = document.querySelectorAll(".card");
+var cardIsFlipped = false;
+var firstCard, secondCard;
+document.getElementById("reset_button").addEventListener("click",resetGame);
+
+
+function flipCard() {
+    if (this===firstCard){
+        return;
+    }
+    
+    this.classList.add('flipped');
+
+    if(!cardIsFlipped){
+        cardIsFlipped=true;
+        firstCard=this;
+        document.getElementById('flips').innerHTML++
+        return;
+    }
+
+    secondCard = this;
+    document.getElementById('flips').innerHTML++
+
+    checkMatch();
+}
+
+function checkMatch(){
+    if (firstCard.dataset.card_type===secondCard.dataset.card_type){
+        cardsMatched();
+    }
+    else{
+        unflipCards()
+    }
+}
+
+//click event listener removed when cards matched
+function cardsMatched(){
+    firstCard.removeEventListener("click",flipCard);
+    secondCard.removeEventListener("click",flipCard);
+    resetStates();
+}
+
+function resetStates(){
+    cardIsFlipped = false;
+    firstCard = undefined;
+    secondCard = undefined;
+}
+
+function unflipCards(){
+    setTimeout(function(){firstCard.classList.remove("flipped");
+    secondCard.classList.remove("flipped");
+    resetStates();},1000);
+}
+
+function resetGame(){
+    for (let x of cards){
+        x.classList.remove("flipped");
+    }
+    addClicksToCards();
+    resetCounter();
+    resetStates();
+    shuffleCards();
+}
+
+function addClicksToCards(){
+    let cards = document.querySelectorAll(".card");
+    for(let x of cards){
+        x.addEventListener("click",flipCard)
+    }
+}
+
+function resetCounter(){
+    document.getElementById('flips').innerHTML=0;
+}
+
+//god bless stackoverflow
+function shuffleArray(array){
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0!==currentIndex){
+        randomIndex = Math.floor(Math.random()*currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+function removeElementsByClass(className){
+    let elements = document.getElementsByClassName(className);
+    while (elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+function shuffleCards(){
+    let cardsArray=["ditto_bulbasaur","ditto_bulbasaur","ditto_pikachu","ditto_pikachu","ditto_quilava","ditto_quilava","feebas","feebas","mimikyu","mimikyu","mudkip","mudkip","quagsire","quagsire","tyranitar","tyranitar"];
+    removeElementsByClass("card");
+    shuffleArray(cardsArray);
+    for (let x of cardsArray){
+        let card_div = document.createElement('div');
+        card_div.className = "card";
+        card_div.dataset.card_type = x;
+
+        let cardfront = document.createElement('img');
+        cardfront.className = "card_front";
+        cardfront.src = x+".png";
+
+        let cardback = document.createElement('img');
+        cardback.className = "card_back";
+        cardback.src = "cardback.png";
+
+        card_div.append(cardfront);
+        card_div.append(cardback);
+        game_board.appendChild(card_div);
+    }
+    addClicksToCards();
+}
+
+addClicksToCards();
+shuffleCards();
